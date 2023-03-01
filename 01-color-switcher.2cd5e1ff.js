f2b3ac436e847cba5563df778c6d5a3b9e36c2af
DenNaumenko <45893954+DenNaumenko@users.noreply.github.com>
2023-03-01 15:57:48 +0000

    Deploying to gh-pages from @ DenNaumenko/goit-js-hw-09@265a043cfd65fcc7f03f4b1edfbde15714aab6cb 🚀

---
+++ 01-color-switcher.2cd5e1ff.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3),e.setAttribute("disabled",!0),r.removeAttribute("disabled")})),r.addEventListener("click",(function(){clearInterval(t),r.setAttribute("disabled",!0),e.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.2cd5e1ff.js.map
